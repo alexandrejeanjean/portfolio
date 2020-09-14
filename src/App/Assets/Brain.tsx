5d68371912b934300aa74ff5bc8e3ea88ecda874
alex <alex@MacBook-Pro-de-alex.local>
2020-09-14 20:31:47 +0200

    [Performance] Testing some performance ajustements

---
+++ src/App/Assets/Brain.tsx
@@ -0,0 +1,216 @@
+import * as React from 'react'
+
+function SvgComponent(props: any) {
+  return (
+    <svg viewBox='0 0 1429 1109' {...props}>
+      <linearGradient
+        id='prefix__a'
+        gradientUnits='userSpaceOnUse'
+        x1={596.404}
+        y1={726.248}
+        x2={769.118}
+        y2={553.534}
+        gradientTransform='matrix(.9987 .05007 -.05007 .9987 33.22 -32.279)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M631 486.3c13.3-18.5 49.6-23.7 68.5-8.6 7.6 6.1 11.8 15 14.6 28.8 7.8 38.6-5.9 65.9-15.1 101.4-10 38.7-9.8 68.8-9.6 111.7.4 70.7 14.9 109.4 6.4 112.5-9.5 3.4-34.8-41.9-48.4-72.5-13.5-30.3-28-63-26.8-105.5 1.2-42.2 16.4-42.7 16.3-85.3-.2-47.1-18.8-64.5-5.9-82.5z'
+        opacity={0.35}
+        fill='url(#prefix__a)'
+      />
+      <linearGradient
+        id='prefix__b'
+        gradientUnits='userSpaceOnUse'
+        x1={588.404}
+        y1={721.248}
+        x2={761.118}
+        y2={548.534}
+        gradientTransform='matrix(.9987 .05007 -.05007 .9987 33.22 -32.279)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M623.3 480.9c13.3-18.5 49.6-23.7 68.5-8.6 7.6 6.1 11.8 15 14.6 28.8 7.8 38.6-5.9 65.9-15.1 101.4-10 38.7-9.8 68.8-9.6 111.7.4 70.7 14.9 109.4 6.4 112.5-9.5 3.4-34.8-41.9-48.4-72.5-13.5-30.3-28-63-26.8-105.5 1.2-42.2 16.4-42.7 16.3-85.3-.2-47.1-18.9-64.4-5.9-82.5z'
+        fill='url(#prefix__b)'
+      />
+      <linearGradient
+        id='prefix__c'
+        gradientUnits='userSpaceOnUse'
+        x1={689.962}
+        y1={698.393}
+        x2={862.676}
+        y2={525.679}
+        gradientTransform='matrix(.9994 .03419 -.03419 .9994 22.047 -25.575)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M722.5 458.8c13-18.7 49.3-24.5 68.4-9.7 7.7 6 12.1 14.8 15 28.5 8.4 38.5-4.9 66-13.5 101.6-9.4 38.9-8.8 68.9-7.8 111.8 1.5 70.7 16.7 109.1 8.2 112.3-9.5 3.6-35.5-41.4-49.6-71.7-14-30.1-29-62.5-28.4-105 .5-42.2 15.8-43 14.9-85.5-1-47-19.9-64.1-7.2-82.3z'
+        opacity={0.35}
+        fill='url(#prefix__c)'
+      />
+      <linearGradient
+        id='prefix__d'
+        gradientUnits='userSpaceOnUse'
+        x1={681.962}
+        y1={693.393}
+        x2={854.676}
+        y2={520.679}
+        gradientTransform='matrix(.9994 .03419 -.03419 .9994 22.047 -25.575)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M714.6 453.5c13-18.7 49.3-24.5 68.4-9.7 7.7 6 12.1 14.8 15 28.5 8.4 38.5-4.9 66-13.5 101.6-9.4 38.9-8.8 68.9-7.8 111.8 1.5 70.7 16.7 109.1 8.2 112.3-9.5 3.6-35.5-41.4-49.6-71.7-14-30.1-29-62.5-28.4-105 .5-42.2 15.8-43 14.9-85.5-.9-47-19.8-64-7.2-82.3z'
+        fill='url(#prefix__d)'
+      />
+      <linearGradient
+        id='prefix__e'
+        gradientUnits='userSpaceOnUse'
+        x1={497.404}
+        y1={760.248}
+        x2={670.118}
+        y2={587.534}
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M524 522c12.3-19.1 48.4-26.1 68-12 7.9 5.7 12.6 14.4 16 28 9.7 38.2-2.6 66.2-10 102-8.1 39.2-6.4 69.2-4 112 3.9 70.6 20.4 108.5 12 112-9.3 3.9-36.9-40.1-52-70-15-29.6-31.1-61.5-32-104-.9-42.2 14.3-43.5 12-86-2.5-47-22-63.3-10-82z'
+        opacity={0.35}
+        fill='url(#prefix__e)'
+      />
+      <linearGradient
+        id='prefix__f'
+        gradientUnits='userSpaceOnUse'
+        x1={489.404}
+        y1={755.248}
+        x2={662.118}
+        y2={582.534}
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M516 517c12.3-19.1 48.4-26.1 68-12 7.9 5.7 12.6 14.4 16 28 9.7 38.2-2.6 66.2-10 102-8.1 39.2-6.4 69.2-4 112 3.9 70.6 20.4 108.5 12 112-9.3 3.9-36.9-40.1-52-70-15-29.6-31.1-61.5-32-104-.9-42.2 14.3-43.5 12-86-2.5-47-22-63.3-10-82z'
+        fill='url(#prefix__f)'
+      />
+      <linearGradient
+        id='prefix__g'
+        gradientUnits='userSpaceOnUse'
+        x1={1271.827}
+        y1={1105.515}
+        x2={1315.88}
+        y2={1061.461}
+        gradientTransform='rotate(134.999 1024.401 436.094)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={376.1}
+        cy={168.9}
+        r={31.2}
+        opacity={0.39}
+        fill='url(#prefix__g)'
+      />
+      <linearGradient
+        id='prefix__h'
+        gradientUnits='userSpaceOnUse'
+        x1={1622.716}
+        y1={955.99}
+        x2={1688.796}
+        y2={889.909}
+        gradientTransform='rotate(134.999 1024.401 436.094)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={233.7}
+        cy={538.3}
+        r={46.7}
+        opacity={0.39}
+        fill='url(#prefix__h)'
+      />
+      <linearGradient
+        id='prefix__i'
+        gradientUnits='userSpaceOnUse'
+        x1={1019.089}
+        y1={308.487}
+        x2={1041.116}
+        y2={286.46}
+        gradientTransform='rotate(134.999 1024.401 436.094)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={1118.4}
+        cy={538.1}
+        r={15.6}
+        opacity={0.39}
+        fill='url(#prefix__i)'
+      />
+      <linearGradient
+        id='prefix__j'
+        gradientUnits='userSpaceOnUse'
+        x1={763.707}
+        y1={536.691}
+        x2={851.815}
+        y2={448.584}
+        gradientTransform='rotate(134.999 1024.401 436.094)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={1137.6}
+        cy={242.9}
+        r={62.3}
+        opacity={0.39}
+        fill='url(#prefix__j)'
+      />
+      <g>
+        <linearGradient
+          id='prefix__k'
+          gradientUnits='userSpaceOnUse'
+          x1={328.11}
+          y1={442.065}
+          x2={1061.884}
+          y2={442.065}
+        >
+          <stop offset={0} stopColor='#daf8ff' />
+          <stop offset={0.999} stopColor='#b3d4fc' />
+        </linearGradient>
+        <path
+          d='M330.1 468.2c17.4-144.8 156-253.7 286.7-286.7 27.9-7 111.4-26.9 207.9 6.3C928.4 223.5 1049.3 321.3 1061 465c6.7 82.5-20.3 209.6-100.8 242.6-106.2 43.5-194.7-122.3-365.4-94.5-99.8 16.3-135.6 83.7-192.2 59.9-61.1-25.8-81.4-130.8-72.5-204.8z'
+          fill='url(#prefix__k)'
+        />
+        <linearGradient
+          id='prefix__l'
+          gradientUnits='userSpaceOnUse'
+          x1={342.11}
+          y1={450.065}
+          x2={1075.884}
+          y2={450.065}
+        >
+          <stop offset={0} stopColor='#daf8ff' />
+          <stop offset={0.999} stopColor='#b3d4fc' />
+        </linearGradient>
+        <path
+          d='M344.1 476.2c17.4-144.8 156-253.7 286.7-286.7 27.9-7 111.4-26.9 207.9 6.3C942.4 231.5 1063.3 329.3 1075 473c6.7 82.5-20.3 209.6-100.8 242.6-106.2 43.5-194.7-122.3-365.4-94.5-99.8 16.3-135.6 83.7-192.2 59.9-61.1-25.8-81.4-130.8-72.5-204.8z'
+          opacity={0.39}
+          fill='url(#prefix__l)'
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default SvgComponent

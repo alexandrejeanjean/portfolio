3f3f42623a86e0f9e16bbc025217f72061fb727d
alex <alex@MacBook-Pro-de-alex.local>
2020-09-19 14:05:22 +0200

    [Svg background] added some svg background

---
+++ src/App/Assets/BrainHead.tsx
@@ -0,0 +1,121 @@
+import * as React from 'react'
+
+type Props = { className: string }
+
+function SvgComponent(props: Props) {
+  return (
+    <svg viewBox='0 0 831 612' {...props}>
+      <linearGradient
+        id='prefix__a'
+        gradientUnits='userSpaceOnUse'
+        x1={186}
+        y1={332.118}
+        x2={788.506}
+        y2={332.118}
+        gradientTransform='matrix(1 0 0 -1 0 702)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M187.6 391.4C201.9 272.5 315.7 183.1 423 156c22.9-5.7 91.5-22.1 170.7 5.2 85.1 29.3 184.4 109.6 194 227.6 5.5 67.7-16.7 172.1-82.8 199.2-87.2 35.7-159.8-100.4-300-77.6-81.9 13.3-111.3 68.6-157.7 49.1-50.2-21.2-66.9-107.4-59.6-168.1z'
+        fill='url(#prefix__a)'
+      />
+      <linearGradient
+        id='prefix__b'
+        gradientUnits='userSpaceOnUse'
+        x1={197.494}
+        y1={325.55}
+        x2={800}
+        y2={325.55}
+        gradientTransform='matrix(1 0 0 -1 0 702)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <path
+        d='M199.1 397.9C213.4 279 327.2 189.6 434.5 162.5c22.9-5.7 91.5-22.1 170.7 5.2 85.1 29.3 184.4 109.6 194 227.6 5.5 67.7-16.7 172.1-82.8 199.2-87.2 35.7-159.8-100.4-300-77.6-81.9 13.4-111.3 68.7-157.8 49.2-50.1-21.2-66.8-107.4-59.5-168.2z'
+        opacity={0.39}
+        fill='url(#prefix__b)'
+      />
+      <linearGradient
+        id='prefix__c'
+        gradientUnits='userSpaceOnUse'
+        x1={979.617}
+        y1={-638.811}
+        x2={1023.67}
+        y2={-594.758}
+        gradientTransform='scale(-1 1) rotate(45 -569.992 -1743.1)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={255.1}
+        cy={164.7}
+        r={31.2}
+        opacity={0.39}
+        fill='url(#prefix__c)'
+      />
+      <linearGradient
+        id='prefix__d'
+        gradientUnits='userSpaceOnUse'
+        x1={1203.277}
+        y1={-698.562}
+        x2={1269.322}
+        y2={-632.518}
+        gradientTransform='scale(-1 1) rotate(45 -569.992 -1743.1)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={54.7}
+        cy={296.1}
+        r={46.7}
+        opacity={0.39}
+        fill='url(#prefix__d)'
+      />
+      <linearGradient
+        id='prefix__e'
+        gradientUnits='userSpaceOnUse'
+        x1={738.893}
+        y1={-573.638}
+        x2={760.92}
+        y2={-551.611}
+        gradientTransform='scale(-1 1) rotate(45 -569.992 -1743.1)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={471.4}
+        cy={24.9}
+        r={15.6}
+        opacity={0.39}
+        fill='url(#prefix__e)'
+      />
+      <linearGradient
+        id='prefix__f'
+        gradientUnits='userSpaceOnUse'
+        x1={575.431}
+        y1={-322.449}
+        x2={663.537}
+        y2={-234.342}
+        gradientTransform='scale(-1 1) rotate(45 -569.992 -1743.1)'
+      >
+        <stop offset={0} stopColor='#daf8ff' />
+        <stop offset={0.999} stopColor='#b3d4fc' />
+      </linearGradient>
+      <circle
+        cx={764.6}
+        cy={133.7}
+        r={62.3}
+        opacity={0.39}
+        fill='url(#prefix__f)'
+      />
+    </svg>
+  )
+}
+
+export default SvgComponent

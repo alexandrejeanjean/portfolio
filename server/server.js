e2fc0fe786c0959b444a851fd668c8b3384a8707
Home <home@localhost.localdomain>
2020-10-13 15:45:12 +0200

    [Server] Fix bug on server for heroku

--- server/server.js
+++ server/server.js
@@ -1,10 +1,8 @@
 /* Simple server to serve on heroku */
 const path = require("path");
-const helmet = require("helmet");
 const express = require("express");
 const sslRedirect = require("heroku-ssl-redirect").default;
 const app = express();
-app.use(helmet());
 
 const publicPath = path.join(__dirname, "..", "build");
 const port = process.env.PORT || 3000;
